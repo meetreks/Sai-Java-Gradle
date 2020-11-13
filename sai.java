a724e0f1103ed01abb66e6f947ecf89f158cbcc8
rekharamesh <meetreks@gmail.com>
2020-11-13 23:04:35 +0000

    Om Sai Ram

---
+++ sai.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class sai {
+    public static void main(String[] args) throws IOException {
+
+
+        System.out.println("OmSairam - Gradle");
+
+    }
+}
+
